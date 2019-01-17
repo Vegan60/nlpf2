4f27637811e23836a8388fc2df096edae6858d25
Anas Haloui <anashaloui@MacBook-Pro-de-Anas.local>
2019-01-17 14:31:36 +0100

    Login & register

--- nlpf2/urls.py
+++ nlpf2/urls.py
@@ -13,9 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
+from django.urls import path
+from website import views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
+    url(r'^$',views.index,name='index'),
+    url(r'^special/',views.special,name='special'),
+    url(r'^website/',include('website.urls')),
+    url(r'^logout/$', views.user_logout, name='logout'),
 ]
