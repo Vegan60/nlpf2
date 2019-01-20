320777d99351c58b00d8f12612798e9a558b9a6d
basmakamel <basma.kamel@epita.fr>
2019-01-20 15:25:03 +0100

    Conflits in setttings and urls

--- nlpf2/settings.py
+++ nlpf2/settings.py
@@ -57,11 +57,8 @@ ROOT_URLCONF = 'nlpf2.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-<<<<<<< HEAD
-        'DIRS': [os.path.join(BASE_DIR, 'bob/templates')],
-=======
+
         'DIRS': [TEMPLATE_DIR],
->>>>>>> 9439220
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -126,10 +123,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-<<<<<<< HEAD
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-LOGIN_REDIRECT_URL = '/'
-=======
 STATICFILES_DIRS = [STATIC_DIR,]
 
 MEDIA_ROOT = MEDIA_DIR
@@ -140,5 +133,3 @@ LOGIN_URL = '/website/user_login/'
 #TEMPLATE_DIRS = (
  #   BASE_DIR + '/templates/',
 #)
-
->>>>>>> 9439220
--- nlpf2/urls.py
+++ nlpf2/urls.py
@@ -13,16 +13,6 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-
-from django.contrib import admin
-from django.urls import include, path
-from django.views.generic.base import TemplateView
-
-    path('bob/', include('bob.urls')),
-    path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')), 
-    path('', TemplateView.as_view(template_name='home.html'), name='home')
-
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.urls import path
